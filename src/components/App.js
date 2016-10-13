1316eddf42a7aaf4a1d7ebbff2717b154f1ca257
Meuss <thomas.miller147@gmail.com>
2016-10-13 12:32:44 +0200

    loading sample data into state onclick, part 14

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
     
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
     // get initial state
     this.state = {
       fishes: {},
       order: {}
     };
   }
+
   addFish(fish) {
     // update our state
     const fishes = {...this.state.fishes};
@@ -23,6 +26,13 @@ class App extends React.Component {
     // set state
     this.setState({ fishes });
   }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -30,7 +40,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
